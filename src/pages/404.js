3d16a2681440b3f57943655a5ccb05c6aeb93a82
Sherif Meimari <sherifmeimari@gmail.com>
2021-10-31 15:42:58 +0200

    add: added starter pack

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const NotFoundPage = () => {
+
+    return (
+        <div>
+            <p>404</p>
+        </div>
+    )
+}
+
+
+export default NotFoundPage
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import '../styles/styles.scss'
+
+const IndexPage = () => (
+  <p>Home</p>
+)
+
+export default IndexPage
